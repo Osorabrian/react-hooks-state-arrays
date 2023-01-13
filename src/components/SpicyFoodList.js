d972701d805462b1fe64f84f5b6facc12250f9bd
Osorabrian <osorabrian@gmail.com>
2023-01-13 17:52:58 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,11 +3,50 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  // const [filterBy, setFilterBy] = useState("All");
+
+// const foodsToDisplay = foods.filter((food) => {
+//   if (filterBy === "All") {
+//     return true;
+//   } else {
+//     return food.cuisine === filterBy;
+//   }
+// });
+
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+    const newFoods = [...foods, newFood]
+    setFoods(newFoods)
   }
+
+
+  // function handleLiClick(id) {
+  //   const newFoodArray = foods.filter((food) => food.id !== id);
+  //   setFoods(newFoodArray);
+  // }
+  
+  // function handleLiClick(id) {
+  //   const newFoodArray = foods.map((food) => {
+  //     if (food.id === id) {
+  //       return {
+  //         ...food,
+  //         heatLevel: food.heatLevel + 1,
+  //       };
+  //     } else {
+  //       return food;
+  //     }
+  //   });
+  //   setFoods(newFoodArray);
+  // }
+
+  // const foodList = foodsToDisplay.map((food) => (
+  //   <li key={food.id} onClick={() => handleLiClick(food.id)}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+
 
   const foodList = foods.map((food) => (
     <li key={food.id}>
@@ -15,12 +54,28 @@ function SpicyFoodList() {
     </li>
   ));
 
+  // const foodList = foods.map((food) => (
+  //   <li key={food.id} onClick={() => handleLiClick(food.id)}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+
+// function handleFilterChange(event) {
+//     setFilterBy(event.target.value);
+//   }
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      {/* <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select> */}
     </div>
   );
 }
-
 export default SpicyFoodList;
